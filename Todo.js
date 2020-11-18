c61826f9e1193852b6cef536824a5cc35c3f9ba9
yoneda <nsshowdream@gmail.com>
2020-11-18 15:35:21 +0900

    [fix] make it possible to remove a task

--- Todo.js
+++ Todo.js
@@ -17,26 +17,47 @@ import {
 } from "easy-peasy";
 
 const store = createStore({
-  tasks: ["aaa", "bbb", "ccc"],
+  tasks: ["aaa", "bbb", "ccc", "ddd", "eee"],
   add: action((state, payload) => {
     state.tasks.push(payload);
+  }),
+  remove: action((state, payload) => {
+    state.tasks.splice(payload, 1);
   }),
 });
 
 function Todo() {
   const tasks = useStoreState((state) => state.tasks);
-  const add = useStoreActions((actions) => actions.add);
+  const [add, remove] = useStoreActions((actions) => [
+    actions.add,
+    actions.remove,
+  ]);
   const [text, setText] = useState("");
   const onClick = () => {
     add(text);
     setText("");
   };
+  const onRemove = (index) => {
+    remove(index);
+  };
   return (
     <section id="todo">
-      <input type="text" value={text} onChange={e=>setText(e.target.value)}/>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
       <button onClick={onClick}>+</button>
-      {tasks.map((task, index) => (
-        <p key={index}>{task}</p>
+      {tasks.map((task, key) => (
+        <p key={key}>
+          {task}
+          <span
+            onClick={() => onRemove(key)}
+            style={{ cursor: "pointer", color: "crimson" }}
+          >
+            x
+          </span>
+        </p>
       ))}
     </section>
   );
